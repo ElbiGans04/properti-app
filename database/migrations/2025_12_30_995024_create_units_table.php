5f135ad0f1d0ed4fdbf6c461a8a090a55a4d8dc4
elbi <rhafaelbijaksana04@gmail.com>
2025-12-31 16:19:35 +0700

    feat: membuat unit & image migrations beserta model

--- database/migrations/2025_12_30_995024_create_units_table.php
+++ database/migrations/2025_12_30_995024_create_units_table.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Kecamatan;
+use App\Models\Kota;
 use App\Models\Marketing;
+use App\Models\Provinsi;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,7 +17,7 @@ return new class extends Migration
     {
         Schema::create('units', function (Blueprint $table) {
             $table->id();
-            $table->enum('jenis_transaksi', ['JUAL', 'SEWA']);
+            $table->enum('jenis_transaksi', ['JUAL', 'SEWA', 'JUAL ATAU SEWA']);
             $table->enum('jenis_unit', ['APARTEMENT', 'GUDANG', 'PABRIK', 'RUKO', 'TANAH', 'LAINNYA']);
             $table->boolean('is_lelang')->default(false);
             $table->string('judul')->required();
@@ -24,16 +27,19 @@ return new class extends Migration
             $table->enum('legalitas', ['SHM', 'SHGB', 'GIRIK', 'AJB', 'SHGU', 'TRADISIONAL', 'LAINNYA']);
             $table->enum('hadap', ['UTARA', 'BARAT', 'SELATAN', 'TIMUR']);  
             $table->foreignIdFor(Marketing::class);
-            $table->decimal('harga', 15, 2);
-            $table->enum('harga_tipe', ['HARGA_SEWA_PERTAHUN', 'HARGA_SEWA_PERBULAN', 'HARGA_JUAL'])->default('HARGA_JUAL');
-            $table->string('link_post_fb');
-            $table->string('link_post_ig');
-            $table->string('link_post_yt');
-            $table->string('link_post_tt');
-            $table->text('note');
+            $table->decimal('harga_jual', 15, 2)->nullable();
+            $table->decimal('harga_sewa', 15, 2)->nullable();
+            $table->enum('harga_sewa_tipe', ['HARGA_SEWA_PERTAHUN', 'HARGA_SEWA_PERBULAN'])->default('HARGA_SEWA_PERTAHUN')->nullable();
+            $table->string('link_post_fb')->nullable();
+            $table->string('link_post_ig')->nullable();
+            $table->string('link_post_yt')->nullable();
+            $table->string('link_post_tt')->nullable();
+            $table->text('note')->nullable();
             $table->string('link_maps');
             $table->text('alamat');
-            
+            $table->foreignIdFor(Kecamatan::class);
+            $table->foreignIdFor(Kota::class);
+            $table->foreignIdFor(Provinsi::class);
             $table->timestamps();
         });
     }
